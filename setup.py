0b333ef0130251df38cd97c4d6ef1098a78d9324
sdcoffey <steve.coffey@getbraintree.com>
2017-02-27 17:18:39 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+version = "0.1"
+
+long_description = """
+	BraintreeHttp is a generic http client designed to be used with code-generated projects.
+"""
+
+setup(
+    name="braintreehttp",
+    long_description=long_description,
+    version=version,
+    author="Braintree",
+    author_email="support@braintreepayments.com",
+    packages=["braintreehttp"],
+    install_requires=['requests>=2.0.0', 'six>=1.0.0', 'pyopenssl>=0.15'],
+    liscene="MIT",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
--- test/unit_test_utils.py
+++ test/unit_test_utils.py
@@ -1,10 +1,4 @@
 import responses
-import json
-import requests
-
-OAUTH_PATH = "/v1/oauth2/token"
-OPENID_CONNECT_PATH = "/v1/identity/openidconnect/tokenservice"
-
 
 def stub_request_with_empty_reponse(request):
     stub_request_with_response(request, "{}", 200)
@@ -14,29 +8,3 @@ def stub_request_with_response(request, json, status):
     responses.add(request.method, request.url, body=json, content_type="application/json", status=status)
 
 
-def stub_access_token_request(access_token_json=None):
-    if not access_token_json:
-        access_token_json = access_token_response()
-
-    responses.add("POST", "http://localhost/v1/oauth2/token", body=access_token_json, status=200)
-
-
-def access_token_request(refresh_token=None):
-    url = "http://localhost"
-    if refresh_token:
-        url += "/v1/identity/openidconnect/tokenservice"
-    else:
-        url += "/v1/oauth2/token"
-    return requests.Request(method="POST", url=url)
-
-
-def access_token_response(refresh_token=None):
-    resp = {
-        "access_token": "sample-access-token",
-        "expires_in": 3600,
-        "token_type": "Bearer"
-    }
-    if refresh_token:
-        resp["refresh_token"] = refresh_token
-
-    return json.dumps(resp)
